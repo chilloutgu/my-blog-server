a4224110d67152182b6f4bca80b37e4101a18f83
김동구 <bbonggu92@gmail.com>
2021-01-25 21:19:46 +0900

    add movie.service

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,11 +1,17 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
 
+  constructor(private readonly moviesService: MoviesService) {
+
+  }
+
   @Get()
-  getAll(): string {
-    return "this will return all movies";
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
   }
 
   @Get("/search")
@@ -15,7 +21,7 @@ export class MoviesController {
 
   @Get("/:id")
   getOne(@Param("id") movieId: string): string {
-    return `this will return one movie wtih the id: ${movieId}`;
+    return this.moviesService.getOne(movieId);
   }
 
   @Post()
