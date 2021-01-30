bd2e389afb4b17158f4be51df8da6bb820318062
김동구 <bbonggu92@gmail.com>
2021-01-30 13:54:02 +0900

    [add] create ApiReponse object

---
+++ src/shared/object/api.object.ts
@@ -0,0 +1,22 @@
+import { HttpStatus } from "@nestjs/common";
+
+export class ApiResponse<T> {
+  private httpStatus: HttpStatus;
+  private message: ResponseMessage;
+  private data?: T | undefined;
+
+  public constructor(httpStatus: HttpStatus, message: ResponseMessage) {
+    this.httpStatus = httpStatus;
+    this.message = message;
+  }
+
+  public setData(data: T | undefined): ApiResponse<T> {
+    this.data = data;
+    return this;
+  }
+}
+
+export enum ResponseMessage {
+  SUCCESS = 'success',
+  FAILURE = 'failure'
+}
--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Patch, Post, Delete, Body, Param, ParseIntPipe, HttpCode, ClassSerializerInterceptor, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Patch, Post, Delete, Body, Param, ParseIntPipe, HttpCode, ClassSerializerInterceptor, UseInterceptors, HttpStatus } from '@nestjs/common';
+import { ApiResponse, ResponseMessage } from 'src/shared/object/api.object';
 import { CreateUserDTO } from '../dto/create-user.dto';
 import { UpdateUserDTO } from '../dto/update-user.dto';
 import { User } from '../entity/user.entity';
@@ -10,34 +11,41 @@ export class UserController {
 
   @Post()
   @HttpCode(201)
-  public async create(@Body() createUserDTO: CreateUserDTO): Promise<void> {
+  public async create(@Body() createUserDTO: CreateUserDTO): Promise<ApiResponse<User>> {
     const newUser = createUserDTO.toEntity();
     await this.userService.create(newUser);
+    return new ApiResponse<User>(HttpStatus.CREATED, ResponseMessage.SUCCESS);
   }
 
   @Get()
   @HttpCode(200)
   @UseInterceptors(ClassSerializerInterceptor)
-  public async findAll(): Promise<User[] | undefined> {
-      return await this.userService.findAll();
+  public async findAll(): Promise<ApiResponse<User[]>> {
+      const foundUsers = await this.userService.findAll();
+      return new ApiResponse<User[]>(HttpStatus.OK, ResponseMessage.SUCCESS)
+        .setData(foundUsers);
   }
 
   @Get(':id')
   @HttpCode(200)
   @UseInterceptors(ClassSerializerInterceptor)
-  public async findOne(@Param('id') id: string): Promise<User | undefined> {
-    return await this.userService.findOne(id);
+  public async findOne(@Param('id') id: string): Promise<ApiResponse<User>> {
+    const foundUser = await this.userService.findOne(id);
+    return new ApiResponse<User>(HttpStatus.OK, ResponseMessage.SUCCESS)
+      .setData(foundUser);
   }
 
   @Patch(':id')
   @HttpCode(200)
-  public async modify(@Param('id') id: string, @Body() updateUserDTO: UpdateUserDTO):Promise<void> {
+  public async modify(@Param('id') id: string, @Body() updateUserDTO: UpdateUserDTO):Promise<ApiResponse<User>> {
     await this.userService.modify(id, updateUserDTO);
+    return new ApiResponse<User>(HttpStatus.OK, ResponseMessage.SUCCESS);
   }
 
   @Delete(':id')
   @HttpCode(200) 
-  public async remove(@Param('id') id: string): Promise<void> {
+  public async remove(@Param('id') id: string): Promise<ApiResponse<User>> {
     await this.userService.remove(id);
+    return new ApiResponse<User>(HttpStatus.OK, ResponseMessage.SUCCESS);
   }
 }
